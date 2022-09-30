7da267c0567b109f9f4151bdb9cd083bdec34bd2
GameBayoumy <xilionhero@gmail.com>
2022-09-30 15:41:07 +0200

    Oranize Speckle example

---
+++ Packages/systems.speckle.speckle-unity/RecursiveConverter.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+using Objects.Converter.Unity;
+using Speckle.Core.Kits;
+using UnityEngine;
+
+namespace Speckle.ConnectorUnity
+{
+    /// <summary>
+    /// <see cref="Component"/> for recursive conversion of Speckle Objects to Native, and Native Objects to Speckle
+    /// </summary>
+    [AddComponentMenu("Speckle/Conversion/" + nameof(RecursiveConverter))]
+    [ExecuteAlways, DisallowMultipleComponent]
+    public partial class RecursiveConverter : MonoBehaviour
+    {
+        public virtual ISpeckleConverter ConverterInstance { get; set; } = new ConverterUnity();
+        
+    }
+}
---
+++ Packages/systems.speckle.speckle-unity/Streams.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Speckle.Core.Api;
+using Speckle.Core.Credentials;
+using Speckle.Core.Logging;
+
+namespace Speckle.ConnectorUnity
+{
+  public static class Streams
+  {
+    public static async Task<List<Stream>> List(int limit = 10)
+    {
+      var account = AccountManager.GetDefaultAccount();
+      if (account == null)
+        return new List<Stream>();
+      var client = new Client(account);
+
+      var res = await client.StreamsGet(limit);
+
+      return res;
+    }
+
+    public static async Task<Stream> Get(string streamId, int limit = 10)
+    {
+      var account = AccountManager.GetDefaultAccount();
+      if (account == null)
+        return null;
+      var client = new Client(account);
+
+      var res = await client.StreamGet(streamId, limit);
+
+      if (res.branches.items != null)
+      {
+        res.branches.items.Reverse();
+      }
+
+      return res;
+    }
+  }
+}
