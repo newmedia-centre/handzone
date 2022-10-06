7f25c23274b2f0098a575911463e61f537ff3991
GameBayoumy <xilionhero@gmail.com>
2022-10-06 15:18:52 +0200

    Adds new Robot library without the rhino3dm dependency

---
+++ Assets/Robots/Sample/Robot.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Robots.Samples.Unity
+{
+    public class Robot : MonoBehaviour
+    {
+        [SerializeField]
+        
+        #nullable enable
+        Material? _material;
+        Program? _program;
+
+        async void Start()
+        {
+            _program = await TestProgram.CreateAsync();
+
+            if (_material == null)
+                throw new ArgumentNullException(nameof(_material));
+
+            _program.MeshPoser = new UnityMeshPoser(_program.RobotSystem, _material);
+        }
+
+        void Update()
+        {
+            if (_program is null)
+                return;
+
+            var time = Mathf.PingPong(Time.time, (float)_program.Duration);
+            _program.Animate(time, false);
+        }
+    }
+}
