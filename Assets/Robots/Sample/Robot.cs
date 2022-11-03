555c9b221aa1d5a445c00381a69363a1b596b035
GameBayoumy <xilionhero@gmail.com>
2022-11-03 11:22:49 +0100

    Adds new XR interactive toolkit with teleport anchors

--- Assets/Robots/Sample/Robot.cs
+++ Assets/Robots/Sample/Robot.cs
@@ -1,24 +1,31 @@
 using System;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Robots.Samples.Unity
 {
     public class Robot : MonoBehaviour
     {
+        public PlaybackPanel playbackPanel;
+        
         [SerializeField]
         #nullable enable
         private Material? _material;
         private Program? _program;
         private UnityMeshPoser? _meshPoser;
+        private bool isPlaying = false;
 
         void Update()
         {
             if (_program is null)
                 return;
 
-            var time = Mathf.PingPong(Time.time, (float)_program.Duration);
-            _program.Animate(time, false);
+            if (isPlaying)
+            {
+                var time = Mathf.PingPong(Time.time, (float)_program.Duration);
+                _program.Animate(time, false);
+            }
         }
 
         public async void CreateProgramFromJSON(string json)
@@ -32,6 +39,25 @@ namespace Robots.Samples.Unity
                 _meshPoser = new UnityMeshPoser(_program.RobotSystem, _material);
             
             _program.MeshPoser = _meshPoser;
+
+            playbackPanel.playButton.onClick.AddListener(delegate { SetPlayback(true); });
+            playbackPanel.pauseButton.onClick.AddListener(delegate { SetPlayback(false); });
+            playbackPanel.slider.onValueChanged.AddListener(value => SetPlaybackTime(value));
+            playbackPanel.slider.maxValue = (float)_program.Duration;
+        }
+
+        void SetPlayback(bool value)
+        {
+            isPlaying = value;
+            Debug.Log("blup");
+        }
+
+        void SetPlaybackTime(float time)
+        {
+            if (_program is null)
+                return;
+            
+            _program.Animate(time, false);
         }
     }
 }
---
+++ Assets/Scripts/PlaybackPanel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaybackPanel : MonoBehaviour
+{
+    public Button playButton;
+    public Button pauseButton;
+    public Slider slider;
+}
