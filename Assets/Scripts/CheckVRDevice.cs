bf3363e3a931bb267dde4e42483482f238796ae6
jirrivdb <jirrivdb@gmail.com>
2023-01-09 16:55:29 +0100

    Merge issue resolved, CheckVRDevices updated

--- Assets/Scripts/CheckVRDevice.cs
+++ Assets/Scripts/CheckVRDevice.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CheckVRDevice : MonoBehaviour
 {
-<<<<<<< HEAD
     // Check for a VR Device at startup, if none is found activate the movement model
     bool VRCheck;
     void Start()
@@ -18,13 +17,5 @@ public class CheckVRDevice : MonoBehaviour
         {
             Destroy(gameObject);
         }
-=======
-
-    // Update is called once per frame
-    void Update()
-    {
-        var VRCheck = UnityEngine.XR.InputDevices.GetDeviceAtXRNode(UnityEngine.XR.XRNode.Head).isValid;
-        // Debug.Log(VRCheck);
->>>>>>> 2a3d54676d190054490501723a07de2949f8ab10
     }
 }
