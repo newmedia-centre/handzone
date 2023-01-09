16a31c36d70fe222625fbe255ea48245587f6268
GameBayoumy <xilionhero@gmail.com>
2023-01-09 13:56:29 +0100

    Replaces robot mesh from Robot's library completely by HDRobot

--- Assets/Scripts/CheckVRDevice.cs
+++ Assets/Scripts/CheckVRDevice.cs
@@ -6,16 +6,11 @@ using UnityEngine;
 
 public class CheckVRDevice : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
         var VRCheck = UnityEngine.XR.InputDevices.GetDeviceAtXRNode(UnityEngine.XR.XRNode.Head).isValid;
-        Debug.Log(VRCheck);
+        // Debug.Log(VRCheck);
     }
 }
