c49289e70681b8d1c5347651b8b0bce9712abece
GameBayoumy <xilionhero@gmail.com>
2024-01-30 14:53:29 +0100

    Changes package location

--- Assets/Scripts/UI/RadialInteractable.cs
+++ Assets/Scripts/UI/RadialInteractable.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 
-[ExecuteInEditMode]
 public class RadialInteractable : MonoBehaviour
 {
     [Header("UI Control")] 
---
+++ Assets/Scripts/schema/socket/Vector6D.ts
@@ -0,0 +1,29 @@
+/**
+ * Represents a 6D vector consisting of three force components and three torque components.
+ */
+export interface Vector6D {
+    /**
+     * U-axis
+     */
+    u: number;
+    /**
+     * V-axis
+     */
+    v: number;
+    /**
+     * W-axis
+     */
+    w: number;
+    /**
+     * X-axis
+     */
+    x: number;
+    /**
+     * Y-axis
+     */
+    y: number;
+    /**
+     * Z-axis
+     */
+    z: number;
+}
