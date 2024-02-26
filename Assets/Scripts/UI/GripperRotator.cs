eacf6ffc1eec63e64aad72109879e1e4b7bc0928
Timothy Zonnenberg <g.t.zonnenberg@student.tudelft.nl>
2024-02-26 09:58:36 +0100

    Making the Digital UI element on top of the robot instead of hands

---
+++ Assets/Scripts/UI/GripperRotator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+public class GripperRotator : MonoBehaviour
+{
+    public GameObject player;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 delta = transform.position - player.transform.position;
+
+        transform.rotation = Quaternion.LookRotation(delta);
+    }
+}
