377ef58d241b63c597bd06600919d6c19734a767
luukgoossen <luuk.goossen@hotmail.com>
2024-02-29 11:15:11 +0100

    Spawn Virtual Robots

--- src/server/environment.ts
+++ src/server/environment.ts
@@ -8,17 +8,6 @@ const envSchema = {
 	/** Port that this application runs on, defaults to 3000 */
 	WEB_PORT: port().default(3000),
 
-	/** Port used to read messages from the robots */
-	READ_PORT: port().default(30013),
-
-	/** Port used to write messages to the robots */
-	WRITE_PORT: port().default(30003),
-
-	/** Current node environment, defaults to 'development' */
-	NODE_ENV: z
-		.enum(['development', 'production', 'test'])
-		.default('development'),
-
 	/** Path to the config.json file */
 	CONFIG_PATH: z.string().default('config.json'),
 }
@@ -39,12 +28,9 @@ const configSchema = z.object({
 		}).optional()
 	})).optional(),
 
-	/** Array of virtual robot target objects */
-	VIRTUAL_ROBOTS: z.array(z.object({
-		name: z.string(),
-		port: port().default(30003),
-		address: z.string().ip(),
-	})).optional(),
+	/** Virtual polyscope options */
+	DOCKER_OPTIONS: z.any(),
+	MAX_VIRTUAL: z.number().default(3),
 })
 
 // read the json file from the config path
--- src/server/main.ts
+++ src/server/main.ts
@@ -4,8 +4,7 @@ import ViteExpress from 'vite-express'
 import env from './environment'
 import TCPServer from './tcp'
 import initSocket from './socket'
-import { NDIManager } from './ndi'
-import { strFromU8, gzipSync } from 'fflate'
+import { DockerManager } from './docker'
 
 // create express app
 const app = express()
@@ -23,9 +22,45 @@ const server = app.listen(env.WEB_PORT, () => {
 // start the tcp server
 const tcp = new TCPServer()
 
+// start the docker manager
+const docker = new DockerManager()
+docker.requestVirtualRobot().then(robot => {
+	console.log('Virtual Robot:', robot.NetworkSettings.IPAddress)
+})
+
 // start and attach the socket.io server
 const io = initSocket(tcp)
 io.attach(server)
 
 // let vite manage the server
 ViteExpress.bind(app, server).catch(err => console.error(err))
+
+// handle process termination
+const close = () => {
+	// shut down the virtual robots
+	console.log('Shutting down virtual robots...')
+	docker.containers.forEach((container) => {
+		docker.closeVirtualRobot(container.id)
+		console.log('Closed virtual robot:', container.id)
+	})
+
+	// close the robot connections
+	console.log('Shutting down robots...')
+	tcp.connections.forEach((connection, address) => {
+		// kill the ffmpeg process
+		connection.video?.process?.kill()
+
+		// close the socket connection
+		connection.socket.destroy()
+
+		console.log('Closed robot:', address)
+	})
+
+	// close the express server
+	server.close(() => {
+		console.log('Server closed!')
+	})
+}
+
+process.on('SIGINT', close)
+process.on('SIGTERM', close)
