0d57672f1b683649b64edd5fe45e6ad32a52653f
GameBayoumy <xilionhero@gmail.com>
2024-03-14 16:16:18 +0100

    Update Vector6D.cs

--- Socket/Vector6D.cs
+++ Socket/Vector6D.cs
@@ -9,31 +9,31 @@ namespace Schema.Socket
         /// <summary>
         /// U-axis
         /// </summary>
-        public double U { get; set; }
+        public float U { get; set; }
 
         /// <summary>
         /// V-axis
         /// </summary>
-        public double V { get; set; }
+        public float V { get; set; }
 
         /// <summary>
         /// W-axis
         /// </summary>
-        public double W { get; set; }
+        public float W { get; set; }
 
         /// <summary>
         /// X-axis
         /// </summary>
-        public double X { get; set; }
+        public float X { get; set; }
 
         /// <summary>
         /// Y-axis
         /// </summary>
-        public double Y { get; set; }
+        public float Y { get; set; }
 
         /// <summary>
         /// Z-axis
         /// </summary>
-        public double Z { get; set; }
+        public float Z { get; set; }
     }
 }
