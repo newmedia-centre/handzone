bc1da894d8ee5d531f81e88263f18960081bb377
luukgoossen <luuk.goossen@hotmail.com>
2024-03-20 14:17:34 +0100

    Implement Motion and Internals

--- src/Socket/Interfaces.ts
+++ src/Socket/Interfaces.ts
@@ -13,20 +13,3 @@ export type InterfacesSetStandardDigitalOutIn = {
 	/** The signal level (boolean). */
 	b: boolean
 }
-
-/** Calculate the inverse kinematics for a given pose. */
-export type InterfacesGetInverseKinIn = {
-	/** Tool pose. */
-	x: number[]
-	/** The initial joint position for the inverse kinematics calculation. If not provided, the current joint position is used. */
-	qnear?: number[]
-	/** The maximum allowed position error. If not provided, the default value is used. */
-	maxPositionError?: number
-	/** The tool center point (TCP) to use for the inverse kinematics calculation. If not provided, the default TCP is used. */
-	tcp_offset?: string
-}
-
-export type InterfacesGetInverseKinCallback = {
-	/** The inverse kinematics positions */
-	ik: number[]
-}
--- src/Socket/Unity.ts
+++ src/Socket/Unity.ts
@@ -1,4 +1,29 @@
-import type { Vector3D } from './Motion'
+/** Represents a 6D vector consisting of three force components and three torque components. */
+export type Vector6D = {
+	/** X-axis */
+	x: number
+	/** Y-axis */
+	y: number
+	/** Z-axis */
+	z: number
+	/** U-axis */
+	u: number
+	/** V-axis */
+	v: number
+	/** W-axis */
+	w: number
+}
+
+/** Represents a 3D vector consisting of three components */
+export type Vector3D = {
+	/** X-axis */
+	x: number
+	/** Y-axis */
+	y: number
+	/** Z-axis */
+	z: number
+}
+
 /**
  * Represents a 6 degrees of freedom position with position and rotation vectors.
  */
