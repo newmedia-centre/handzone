59320eaf276dcfd831bff51b506dd7daa04315cd
luukgoossen <luuk.goossen@hotmail.com>
2024-03-26 09:51:07 +0100

    Change Description

--- src/Socket/Grasshopper.ts
+++ src/Socket/Grasshopper.ts
@@ -7,7 +7,7 @@ export type GrasshopperProgramIn = {
 }
 
 /**
- * Sends a json payload of the program to Unity to be deserialized as a program.
+ * Receives a json payload of the program from Unity to be deserialized as a program.
  */
 export type GrasshopperProgramOut = {
 	/** The json payload of the IProgram object received from the server. */
