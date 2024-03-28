1bd4818964f56549b4b53b22dc7cae61dfee76c8
luukgoossen <luuk.goossen@hotmail.com>
2024-03-28 13:33:08 +0100

    Fix Issues

--- src/server/socket/motion.ts
+++ src/server/socket/motion.ts
@@ -8,17 +8,17 @@ import type { NamespaceClientToServerEvents, NamespaceServerToClientEvents, Inte
 export const handleMotionEvents = (socket: Socket<NamespaceClientToServerEvents, NamespaceServerToClientEvents, InterServerEvents, NamespaceSocketData>) => {
 
 	// handle the motion:conveyor_pulse_decode event
-	socket.on('motion:conveyor_pulse_decode', (type, A, B) => {
+	socket.on('motion:conveyor_pulse_decode', ({ type, A, B }) => {
 		robots.send(socket.data.robot, `conveyor_pulse_decode(${type},${A},${B})\n`)
 	})
 
 	// handle the motion:encoder_enable_pulse_decode event
-	socket.on('motion:encoder_enable_pulse_decode', (encoder_index, decoder_type, A, B) => {
+	socket.on('motion:encoder_enable_pulse_decode', ({ encoder_index, decoder_type, A, B }) => {
 		robots.send(socket.data.robot, `encoder_enable_pulse_decode(${encoder_index},${decoder_type},${A},${B})\n`)
 	})
 
 	// handle the motion:encoder_enable_set_tick_count event
-	socket.on('motion:encoder_enable_set_tick_count', (encoder_index, range_id) => {
+	socket.on('motion:encoder_enable_set_tick_count', ({ encoder_index, range_id }) => {
 		robots.send(socket.data.robot, `encoder_enable_set_tick_count(${encoder_index},${range_id})\n`)
 	})
 
@@ -33,7 +33,7 @@ export const handleMotionEvents = (socket: Socket<NamespaceClientToServerEvents,
 	})
 
 	// handle the motion:encoder_set_tick_count event
-	socket.on('motion:encoder_set_tick_count', (encoder_index, count) => {
+	socket.on('motion:encoder_set_tick_count', ({ encoder_index, count }) => {
 		robots.send(socket.data.robot, `encoder_set_tick_count(${encoder_index},${count})\n`)
 	})
 
@@ -58,7 +58,7 @@ export const handleMotionEvents = (socket: Socket<NamespaceClientToServerEvents,
 	})
 
 	// handle the motion:force_mode event
-	socket.on('motion:force_mode', (task_frame, selection_vector, wrench, type, limits) => {
+	socket.on('motion:force_mode', ({ task_frame, selection_vector, wrench, type, limits }) => {
 		robots.send(socket.data.robot, `force_mode([${task_frame}],[${selection_vector}],[${wrench}],${type},[${limits}])\n`)
 	})
 
@@ -93,32 +93,32 @@ export const handleMotionEvents = (socket: Socket<NamespaceClientToServerEvents,
 	})
 
 	// handle the motion:movec event
-	socket.on('motion:movec', (pose_via, pose_to, a, v, r, mode) => {
+	socket.on('motion:movec', ({ pose_via, pose_to, a, v, r, mode }) => {
 		robots.send(socket.data.robot, `movec([${pose_via}],[${pose_to}],a=${a},v=${v},r=${r},mode=${mode})\n`)
 	})
 
 	// handle the motion:movej event
-	socket.on('motion:movej', (q, a, v, t, r) => {
+	socket.on('motion:movej', ({ q, a, v, t, r }) => {
 		robots.send(socket.data.robot, `movej([${q}],a=${a},v=${v},t=${t},r=${r})\n`)
 	})
 
 	// handle the motion:movel event
-	socket.on('motion:movel', (pose, a, v, t, r) => {
+	socket.on('motion:movel', ({ pose, a, v, t, r }) => {
 		robots.send(socket.data.robot, `movel([${pose}], a=${a}, v=${v}, t=${t}, r=${r})\n`)
 	})
 
 	// handle the motion:movep event
-	socket.on('motion:movep', (pose, a, v, r) => {
+	socket.on('motion:movep', ({ pose, a, v, r }) => {
 		robots.send(socket.data.robot, `movep([${pose}], a=${a}, v=${v}, r=${r})\n`)
 	})
 
 	// handle the motion:pause_on_error_code event
-	socket.on('motion:pause_on_error_code', (code, argument) => {
+	socket.on('motion:pause_on_error_code', ({ code, argument }) => {
 		robots.send(socket.data.robot, `pause_on_error_code(${code}${argument ? `, ${argument}` : ''})\n`)
 	})
 
 	// handle the motion:position_deviation_warning event
-	socket.on('motion:position_deviation_warning', (enabled, threshold) => {
+	socket.on('motion:position_deviation_warning', ({ enabled, threshold }) => {
 		robots.send(socket.data.robot, `position_deviation_warning(${enabled}${threshold ? `, ${threshold}` : ''})\n`)
 	})
 
@@ -128,12 +128,12 @@ export const handleMotionEvents = (socket: Socket<NamespaceClientToServerEvents,
 	})
 
 	// handle the motion:servoj event
-	socket.on('motion:servoj', (q, a, v, t, lookahead_time, gain) => {
+	socket.on('motion:servoj', ({ q, a, v, t, lookahead_time, gain }) => {
 		robots.send(socket.data.robot, `servoj([${q}], a=${a}, v=${v}${t ? `, t=${t}` : ''}${lookahead_time ? `, lookahead_time=${lookahead_time}` : ''}${gain ? `, gain=${gain}` : ''})\n`)
 	})
 
 	// handle the motion:set_conveyor_tick_count event
-	socket.on('motion:set_conveyor_tick_count', (tick_count, absolute_encoder_resolution) => {
+	socket.on('motion:set_conveyor_tick_count', ({ tick_count, absolute_encoder_resolution }) => {
 		robots.send(socket.data.robot, `set_conveyor_tick_count(${tick_count}${absolute_encoder_resolution ? `, absolute_encoder_resolution=${absolute_encoder_resolution}` : ''})\n`)
 	})
 
@@ -148,12 +148,12 @@ export const handleMotionEvents = (socket: Socket<NamespaceClientToServerEvents,
 	})
 
 	// handle the motion:speedj event
-	socket.on('motion:speedj', (qd, a, t) => {
+	socket.on('motion:speedj', ({ qd, a, t }) => {
 		robots.send(socket.data.robot, `speedj([${qd}],a=${a}${t ? `,t=${t}` : ''})\n`)
 	})
 
 	// handle the motion:speedl event
-	socket.on('motion:speedl', (xd, a, t, aRot) => {
+	socket.on('motion:speedl', ({ xd, a, t, aRot }) => {
 		robots.send(socket.data.robot, `speedl([${xd}],a=${a}${t && `,t=${t}`}${aRot ? `,aRot=${aRot}` : ''})\n`)
 	})
 
@@ -168,7 +168,7 @@ export const handleMotionEvents = (socket: Socket<NamespaceClientToServerEvents,
 	})
 
 	// handle the motion:stopl event
-	socket.on('motion:stopl', (a, aRot) => {
+	socket.on('motion:stopl', ({ a, aRot }) => {
 		robots.send(socket.data.robot, `stopl(${a}${aRot ? `,aRot=${aRot}` : ''})\n`)
 	})
 
@@ -178,12 +178,12 @@ export const handleMotionEvents = (socket: Socket<NamespaceClientToServerEvents,
 	})
 
 	// handle the motion:track_conveyor_circular event
-	socket.on('motion:track_conveyor_circular', (center, ticksPerRevolution, rotateTool, encoderIndex) => {
+	socket.on('motion:track_conveyor_circular', ({ center, ticksPerRevolution, rotateTool, encoderIndex }) => {
 		robots.send(socket.data.robot, `track_conveyor_circular([${center}], ${ticksPerRevolution}, ${rotateTool}${encoderIndex ? `, encoder_index=${encoderIndex}` : ''})\n`)
 	})
 
 	// handle the motion:track_conveyor_linear event
-	socket.on('motion:track_conveyor_linear', (direction, ticksPerMeter, encoderIndex) => {
+	socket.on('motion:track_conveyor_linear', ({ direction, ticksPerMeter, encoderIndex }) => {
 		robots.send(socket.data.robot, `track_conveyor_linear([${direction}], ${ticksPerMeter}${encoderIndex ? `, encoder_index=${encoderIndex}` : ''})\n`)
 	})
 
