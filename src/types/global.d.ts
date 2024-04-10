070624bc5cdd1fa7ebfe9482516d6fb1a5b9b39c
luukgoossen <luuk.goossen@hotmail.com>
2024-04-10 12:40:05 +0200

    Prepare Prisma Client

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -9,6 +9,7 @@ import type {
 } from '@/server/socket/interface'
 import type { RobotManager as RobotManager } from '@/server/robot'
 import type { DockerManager } from '@/server/docker'
+import type { PrismaClient } from '@prisma/client'
 
 // declare global types
 declare global {
@@ -16,4 +17,5 @@ declare global {
 	var namespaces: Map<string, Namespace> // eslint-disable-line no-var
 	var robots: RobotManager // eslint-disable-line no-var
 	var docker: DockerManager // eslint-disable-line no-var
+	var prisma: PrismaClient // eslint-disable-line no-var
 }
