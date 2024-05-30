df33a6aa81a286ef1f17109fd4cc1dbcb5fa6496
Luuk Goossen <l.j.goossen@tudelft.nl>
2024-05-30 15:32:29 +0200

    Add xr zone logo

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,12 +2,22 @@
 import Image from 'next/image'
 
 export const Footer = () => (
-	<footer className='flex items-center justify-center border-t border-300 bg-white p-4'>
+	<footer className='flex items-center justify-center gap-8 border-t border-300 bg-white p-4'>
 		<a href='https://newmediacentre.tudelft.nl'>
 			<Image
 				src='/logo-nmc.png'
 				alt='NewMedia Centre Logo'
 				width={256}
+				height={32}
+				priority
+			/>
+
+		</a>
+		<a href='https://newmediacentre.tudelft.nl/xr'>
+			<Image
+				src='/logo-xr.png'
+				alt='XR-Zone Logo'
+				width={128}
 				height={32}
 				priority
 			/>
