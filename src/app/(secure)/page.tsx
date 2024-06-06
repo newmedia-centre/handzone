0e17dbfe9e7d20eea390b5d78fdfdac943be65ee
Luuk Goossen <l.j.goossen@tudelft.nl>
2024-06-06 16:40:11 +0200

    Remove validate pin

--- src/app/(secure)/page.tsx
+++ src/app/(secure)/page.tsx
@@ -2,8 +2,6 @@
 import { validateRequest } from '@/server/db/auth'
 
 // import components
-import { WelcomeUser } from './welcome'
-import { ValidateVRPin } from './validate-vr-pin'
 import { StudentCalendar, StudentRequestDashboard } from './student'
 import { TeacherCalendar, TeacherRequestDashboard, TeacherCalendarBlockTool, TeacherRobotMonitoringDashboard } from './teacher'
 
@@ -40,12 +38,6 @@ export default async function Page() {
 						<StudentRequestDashboard user={user!} />
 					</div>
 				)}
-			</div>
-			<div className='z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex'>
-				<div className='flex flex-col gap-2 p-2'>
-					<WelcomeUser />
-					<ValidateVRPin />
-				</div>
 			</div>
 		</main>
 	)
