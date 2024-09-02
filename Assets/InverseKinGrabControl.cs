2c9a3d3cfb7249ea84f11fb2fe789b66a5bf6b1a
GameBayoumy <xilionhero@gmail.com>
2024-09-02 13:15:55 +0200

    Removes SetTCP & changes ik parameter to be qactualjoints

--- Assets/InverseKinGrabControl.cs
+++ Assets/InverseKinGrabControl.cs
@@ -4,54 +4,45 @@ using System.Collections.Generic;
 using PimDeWitte.UnityMainThreadDispatcher;
 using Schema.Socket.Internals;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class InverseKinGrabControl : MonoBehaviour
 {
-    public GameObject referencedJoint;
+    [FormerlySerializedAs("RobotTranslator")] public RobotTranslator robotTranslator;
     public SessionClient sessionClient;
-    private  List<double> _newPose = new (6) {0f,0f,0f,0f,0f,0f};
+    private List<double> _newPose = new (6) {0f,0f,0f,0f,0f,0f};
+    [SerializeField] private XRGrabInteractable _grabInteractable;
+
+    private void OnEnable()
+    {
+        _grabInteractable.selectEntered.AddListener(ValidateInverseKinematics);
+    }
 
     void Start()
     {
         sessionClient.OnConnected += () => UnityMainThreadDispatcher.Instance().Enqueue(ValidateNewPosition);
     }
 
+    private void ValidateInverseKinematics(SelectEnterEventArgs selectEnter)
+    {
+        ValidateNewPosition();
+    }
+
     void OnIKSuccessAction()
     {
-        //Update IK pose with _newPose
-        sessionClient.SetTCP(_newPose.ToArray());
-        print("IK Pose Updated");
+        Debug.Log("IK Success");
     }
 
     void ValidateNewPosition()
     {
-        
-        print("Validating New IK Pose");
-        // Update _newPose from referencedJoint's local transform
-        if (referencedJoint)
-        {
-            // _newPose.Clear();
-            print(referencedJoint.ToString());
-            print(referencedJoint.transform.localPosition.ToString());
-            Vector3 position = referencedJoint.transform.localPosition + transform.localPosition;
-            Vector3 rotation = (transform.localRotation * referencedJoint.transform.localRotation).eulerAngles * Mathf.Deg2Rad;
-            Debug.Log("Position is:" + position.ToString());
-            Debug.Log("Rotation is:" + rotation.ToString());
-            _newPose[0] = position.z / 10;
-            _newPose[1] = position.y / 10;
-            _newPose[2] = position.x / 10;
-            _newPose[3] = rotation.x;
-            _newPose[4] = rotation.y;
-            _newPose[5] = rotation.z;
-        }
-        
         // Construct and send IK Data Request
         InternalsGetInverseKinIn ikDataRequest = new InternalsGetInverseKinIn
         {
             MaxPositionError = null,
             Qnear = null,
             TcpOffset = null,
-            X = _newPose
+            X = robotTranslator.qActualJoints
         };
         sessionClient.SendInverseKinematicsRequest(ikDataRequest, OnIKSuccessAction);
     }
