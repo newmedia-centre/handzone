48d36955c9a5632bd8886d699f8f201c07972c7b
GameBayoumy <xilionhero@gmail.com>
2024-09-10 13:37:52 +0200

    Fixes toggle tool digital out

--- Assets/Scripts/Robot/ToggleDigitalTool.cs
+++ Assets/Scripts/Robot/ToggleDigitalTool.cs
@@ -4,8 +4,5 @@ public class ToggleDigitalTool : MonoBehaviour
 {
     private bool _digitalOutput;
 
-    public void ToggleToolDigitalOut()
-    {
-        _digitalOutput = SessionClient.Instance.ToggleToolDigitalOut(_digitalOutput);
-    }
+    
 }
