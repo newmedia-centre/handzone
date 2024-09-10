a5219862d10ef3f9bc8ac6fa1804be6a78620f01
GameBayoumy <xilionhero@gmail.com>
2024-09-10 17:01:11 +0200

    Adds UI design and impl for switching camera views & playback button for GH program

---
+++ Assets/GrasshopperProgramPlayback.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrasshopperProgramPlayback : MonoBehaviour
+{
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _pauseButton;
+    
+    private void OnEnable()
+    {
+        // Subscribe to the PlayProgram and PauseProgram events
+        _playButton.onClick.AddListener(PlayProgram);
+        _pauseButton.onClick.AddListener(PauseProgram);
+        
+        // Set the initial state of the buttons
+        _playButton.gameObject.SetActive(true);
+        _pauseButton.gameObject.SetActive(false);
+    }
+
+    private void PlayProgram()
+    {
+        SessionClient.Instance.PlayProgram();
+        _pauseButton.gameObject.SetActive(true);
+        _playButton.gameObject.SetActive(false);
+    }
+
+    private void PauseProgram()
+    {
+        SessionClient.Instance.PauseProgram();
+        _pauseButton.gameObject.SetActive(false);
+        _playButton.gameObject.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe from the PlayProgram and PauseProgram events
+        _playButton.onClick.RemoveListener(PlayProgram);
+        _pauseButton.onClick.RemoveListener(PauseProgram);
+    }
+}
--- Assets/Scripts/ReadVideoFeed.cs
+++ Assets/Scripts/ReadVideoFeed.cs
@@ -1,10 +1,23 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ReadVideoFeed : MonoBehaviour
 {
     private MeshRenderer _meshRenderer;
-
+    private List<CameraFeed> _cameraFeeds = new();
+    private int _currentCameraFeedIndex;
+    
+    /// <summary>
+    /// This struct is used to store the camera name and the texture of the camera feed.
+    /// </summary>
+    private class CameraFeed
+    {
+        public string CameraName;
+        public Texture2D Texture2D;
+    }
+    
     /// <summary>
     /// This method is called on the frame when a script is enabled just before any of the Update methods are called the first time.
     /// It initializes the MeshRenderer component and subscribes to the OnCameraFeed event from the WebClient.
@@ -13,7 +26,7 @@ public class ReadVideoFeed : MonoBehaviour
     {
         if (SessionClient.Instance)
         {
-            SessionClient.Instance.OnCameraFeed += LoadVideoFeed;
+            SessionClient.Instance.OnCameraFeed += UpdateVideoFeed;
         }
 
         if (TryGetComponent(out _meshRenderer) == false)
@@ -26,15 +39,48 @@ public class ReadVideoFeed : MonoBehaviour
     {
         if(SessionClient.Instance)
         {
-            SessionClient.Instance.OnCameraFeed -= LoadVideoFeed;
+            SessionClient.Instance.OnCameraFeed -= UpdateVideoFeed;
         }
     }
 
-    private void LoadVideoFeed(string cameraName, Texture2D texture2D)
+    private void UpdateVideoFeed(string cameraName, Texture2D texture2D)
     {
-        if (_meshRenderer)
+        // Check if the camera feed already exists in the list
+        foreach (var cameraFeed in _cameraFeeds.Where(cameraFeed => cameraFeed.CameraName == cameraName))
+        {
+            cameraFeed.Texture2D = texture2D;
+            return;
+        }
+
+        // Add the camera feed to the list
+        _cameraFeeds.Add(new CameraFeed {CameraName = cameraName, Texture2D = texture2D});
+        
+        // Set the texture of the MeshRenderer component if there is only one camera feed
+        if(_cameraFeeds.Count == 1)
         {
             _meshRenderer.material.mainTexture = texture2D;
         }
+    }
+
+    private void NextCameraIndex()
+    {
+        _currentCameraFeedIndex++;
+        if (_currentCameraFeedIndex >= _cameraFeeds.Count)
+        {
+            _currentCameraFeedIndex = 0;
+        }
+        
+        _meshRenderer.material.mainTexture = _cameraFeeds[_currentCameraFeedIndex].Texture2D;
+    }
+    
+    private void PreviousCameraIndex()
+    {
+        _currentCameraFeedIndex--;
+        if (_currentCameraFeedIndex < 0)
+        {
+            _currentCameraFeedIndex = _cameraFeeds.Count - 1;
+        }
+        
+        _meshRenderer.material.mainTexture = _cameraFeeds[_currentCameraFeedIndex].Texture2D;
     }
 }
