8d442451455799a5797fced0aa95fd120a13f76c
GameBayoumy <xilionhero@gmail.com>
2024-09-16 16:27:02 +0200

    Adds box colliders to gh objects that are loaded in

--- Assets/Scripts/Server/SessionMeshes.cs
+++ Assets/Scripts/Server/SessionMeshes.cs
@@ -1,61 +1,57 @@
 using Schema.Socket.Grasshopper;
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SessionMeshes : MonoBehaviour
 {
-    public MeshFilter meshRef1;
-    public MeshFilter meshRef2;
-    public MeshFilter meshRef3;
-    public MeshFilter meshRef4;
-    public MeshFilter meshRef5;
-    
-    // Start is called before the first frame update
+    public List<MeshFilter> meshFilters = new();
+
     private void Start()
     {
         if (SessionClient.Instance)
-          SessionClient.Instance.OnGHMeshes += LoadGHMeshes;
-        
-        meshRef1.mesh.Clear();
-        meshRef2.mesh.Clear();
-        meshRef3.mesh.Clear();
-        meshRef4.mesh.Clear();
-        meshRef5.mesh.Clear();
+            SessionClient.Instance.OnGHMeshes += LoadGHMeshes;
+
+        ClearMeshes();
+    }
+
+    private void ClearMeshes()
+    {
+        foreach (var meshFilter in meshFilters)
+        {
+            meshFilter.mesh.Clear();
+        }
     }
 
     private void LoadGHMeshes(GrasshopperMeshesIn meshData)
     {
-        // TODO: Fix arrays that not working when setting mesh references. So now we are setting them one by one.
-        SetMesh(meshRef1.mesh, meshData.Mesh1);
-        SetMesh(meshRef2.mesh, meshData.Mesh2);
-        SetMesh(meshRef3.mesh, meshData.Mesh3);
-        SetMesh(meshRef4.mesh, meshData.Mesh4);
-        SetMesh(meshRef5.mesh, meshData.Mesh5);
+        SetMesh(meshFilters[0], meshData.Mesh1);
+        SetMesh(meshFilters[1], meshData.Mesh2);
+        SetMesh(meshFilters[2], meshData.Mesh3);
+        SetMesh(meshFilters[3], meshData.Mesh4);
+        SetMesh(meshFilters[4], meshData.Mesh5);
     }
 
-    /// <summary>
-    /// Sets the mesh of the mesh filter to the incoming mesh data.
-    /// </summary>
-    /// <param name="mesh"></param>
-    /// <param name="meshData"></param>
-    private void SetMesh(Mesh mesh, MeshData meshData)
+    private void SetMesh(MeshFilter meshFilter, MeshData meshData)
     {
         if (meshData == null)
         {
-            mesh.Clear();
+            meshFilter.mesh.Clear();
             return;
         }
-        
+
         var newMesh = Utility.CreateMeshFromGrasshopperMesh(meshData);
-        mesh.vertices = newMesh.vertices;
-        mesh.triangles = newMesh.triangles;
-        mesh.RecalculateNormals();
-        mesh.RecalculateBounds();
-        
-        // Set the mesh collider to the new mesh bounds
-        // mesh.GetComponent<BoxCollider>().size = mesh.bounds.size;
-        // mesh.GetComponent<BoxCollider>().center = mesh.bounds.center;
-        
-        // TODO: Set the box collider size of the Grasshopper Object
+        meshFilter.mesh.Clear();
+        meshFilter.mesh.vertices = newMesh.vertices;
+        meshFilter.mesh.triangles = newMesh.triangles;
+        meshFilter.mesh.RecalculateNormals();
+        meshFilter.mesh.RecalculateBounds();
+
+        var boxCollider = meshFilter.GetOrAddComponent<BoxCollider>();
+        if (boxCollider)
+        {
+            boxCollider.size = meshFilter.mesh.bounds.size;
+            boxCollider.center = meshFilter.mesh.bounds.center;
+        }
     }
 }
